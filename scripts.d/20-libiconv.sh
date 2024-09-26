f5704b5365d2bc0bcab95742b6d1cd6db77ec990
rainy liu <rainyliusy3@gmail.com>
2024-09-26 23:00:28 +0800

    rollback gnulib of libiconv

--- scripts.d/20-libiconv.sh
+++ scripts.d/20-libiconv.sh
@@ -3,13 +3,16 @@
 SCRIPT_REPO="https://git.savannah.gnu.org/git/libiconv.git"
 SCRIPT_COMMIT="0d94621c1e182f5a13a9504523afcb01ec546b37"
 
+# https://github.com/jellyfin/jellyfin-ffmpeg/pull/466
+GNULIB_COMMIT="d4ec02b3cc70cddaaa5183cc5a45814e0afb2292" # tag v1.0
+
 ffbuild_enabled() {
     return 0
 }
 
 ffbuild_dockerdl() {
     echo "retry-tool sh -c \"rm -rf iconv && git clone '$SCRIPT_REPO' iconv\" && git -C iconv checkout \"$SCRIPT_COMMIT\""
-    echo "cd iconv && retry-tool ./autopull.sh --one-time"
+    echo "cd iconv && retry-tool ./autopull.sh && ./gitsub.sh checkout gnulib \"$GNULIB_COMMIT\""
 }
 
 ffbuild_dockerbuild() {

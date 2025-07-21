e6febc72531fa0b4da869e660d1a86ae244d0a5e
rainy liu <rainyliusy3@gmail.com>
2025-07-21 16:29:25 +0800

    fix

--- build.sh
+++ build.sh
@@ -34,8 +34,8 @@ cat <<EOF >"$BUILD_SCRIPT"
 
     git clone --filter=blob:none --branch='$GIT_BRANCH' '$FFMPEG_REPO' ffmpeg
     cd ffmpeg
-    sed -i 's/SLIBNAME_WITH_MAJOR=.$(SLIBNAME).$(LIBMAJOR)/SLIBNAME_WITH_MAJOR=$(SLIBNAME)/' ./configure
-    sed -i 's/SLIBNAME_WITH_VERSION=.$(SLIBNAME).$(LIBVERSION)/SLIBNAME_WITH_VERSION=$(SLIBNAME)/' ./configure
+    sed -i "s/SLIBNAME_WITH_MAJOR='\$(SLIBNAME)\.\$(LIBMAJOR)'/SLIBNAME_WITH_MAJOR='\$(SLIBNAME)'/g" ./configure
+    sed -i "s/SLIBNAME_WITH_VERSION='\$(SLIBNAME)\.\$(LIBVERSION)'/SLIBNAME_WITH_VERSION='\$(SLIBNAME)'/g" ./configure
 
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS \$FF_CONFIGURE \
         --extra-cflags="\$FF_CFLAGS" --extra-cxxflags="\$FF_CXXFLAGS" --extra-libs="\$FF_LIBS" \
